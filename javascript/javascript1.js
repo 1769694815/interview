60004e5de1b09686947e2eb033ebdfa5d17af84b
徐文 <1769694815@qq.com>
2020-05-28 13:02:28 +0800

    作用域

---
+++ javascript/javascript1.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: xwen
+ * @Date: 2020-05-28 11:16:59
+ * @LastEditTime: 2020-05-28 11:19:49
+ * @LastEditors: xwen
+ * @Description: 作用域
+ */ 
+
+// 作用域是指程序源代码中定义变量的取域，规定了如何查找变量，也就是
+// 确定当前执行对变量的访问权限。
+// JavaScript 采用词法作用域（lexical scoping），也就是静态作用域。
